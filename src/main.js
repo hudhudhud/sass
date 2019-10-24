73211a9881d431102e115a6cf24b5fe12c2381f4
hd <996810472@qq.com>
2019-10-24 15:03:42 +0800

    edit

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ import 'element-ui/lib/theme-chalk/index.css';
 import store from './store'
 import VueAwesomeSwiper from 'vue-awesome-swiper'
 import 'swiper/dist/css/swiper.css' //这里注意具体看使用的版本是否需要引入样式，以及具体位置。
-import './resize.js'
+import './resize.js' //通过设置device-width,initial-scale来适配移动端，这样开发就可直接用px
 Vue.use(VueAwesomeSwiper, /* { default global options } */ )
 
 Vue.config.productionTip = false
